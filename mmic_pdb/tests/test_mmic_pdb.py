43f41b0e27ac39825912fd0e8c0be13f82cc2b01
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-04-13 16:37:34 -0400

    test function for pdbID

--- mmic_pdb/tests/test_mmic_pdb.py
+++ mmic_pdb/tests/test_mmic_pdb.py
@@ -7,6 +7,17 @@ import mmic_pdb
 import pytest
 import sys
 
+
 def test_mmic_pdb_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "mmic_pdb" in sys.modules
+
+
+def test_mmic_pdbid():
+    inp = mmic_pdb.models.PdbFixerInput(
+        pdbid="4EZI",
+        add_atoms="all",
+        keep_hetero="water",
+        std_residues=True,
+    )
+    return mmic_pdb.components.PdbFixerComponent.compute(inp)
