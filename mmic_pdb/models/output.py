d4255519330dd563204cbad024484d46a005c116
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-04-15 14:19:20 -0400

    add fields: miss_resids and nonstd_resids

--- mmic_pdb/models/output.py
+++ mmic_pdb/models/output.py
@@ -1,6 +1,7 @@
 from mmelemental.models import ProcOutput, Molecule
 from .input import PdbFixerInput
 from pydantic import Field
+from typing import Dict, Tuple, List, Optional, Any
 
 __all__ = ["PdbFixerOutput"]
 
@@ -12,4 +13,15 @@ class PdbFixerOutput(ProcOutput):
     molecule: Molecule = Field(
         ...,
         description="Molecular mechanics molecule object. See the :class:``Molecule`` class. ",
+    )
+    miss_resids: Optional[Dict[Tuple[int, int], List[str]]] = Field(
+        None,
+        description="Missing residues in the SEQRES records for which no atom data is present. "
+        "Each key is a tuple consisting of the index of a chain, and the residue index within "
+        "that chain at which new residues should be inserted. The corresponding value is a list "
+        "of the names of residues to insert there.",
+    )
+    nonstd_resids: Optional[List[Tuple[Any, str]]] = Field(
+        None,
+        description="",
     )
